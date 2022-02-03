1a3f55e6949d0aeaf63d0b821917c81592627760
gssim-kr <cremazer@gmail.com>
2022-02-03 09:50:44 +0900

    리액트 Props 실습 테스트 - Button, Input

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,23 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  it('renders component correctly', () => {
+    const { container } = render(<App />);
+
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+
+    const appLogo = screen.getByAltText('logo');
+    expect(appLogo).toBeInTheDocument();
+    expect(appLogo).toHaveAttribute('src','logo.svg');
+
+    // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
+    expect(container.getElementsByTagName('p')).toHaveLength(1);
+    // eslint-disable-next-line testing-library/no-container,testing-library/no-node-access
+    expect(container.getElementsByTagName('p')[0]).toHaveTextContent('Edit src/App.tsx and save'
+        + ' to reload')
+
+    expect(container).toMatchSnapshot();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Styled from 'styled-components';
+
+import { Button } from 'Components';
+
+const Container = Styled.div`
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+const Contents = Styled.div`
+  display: flex;
+  background-color: #FFFFFF;
+  flex-direction: column;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+`;
+const InputContainer = Styled.div`
+  display: flex;
+`;
+const Input = Styled.input`
+  font-size: 16px;
+  padding: 10px 10px;
+  border-radius: 8px;
+  border: 1px solid #BDBDBD;
+  outline: none;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Contents>
+        <InputContainer>
+          <Input placeholder="할 일을 입력해 주세요" />
+          <Button label="추가" />
+        </InputContainer>
+      </Contents>
+    </Container>
   );
 }
 

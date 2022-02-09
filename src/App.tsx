343956033e35e1e4f294155f1c39705422feb163
gssim-kr <cremazer@gmail.com>
2022-02-09 17:52:56 +0900

    리액트 Props 실습 테스트 - state 실습, Input 컴포넌트에 value 적용

--- src/App.tsx
+++ src/App.tsx
@@ -22,16 +22,43 @@ const InputContainer = Styled.div`
   display: flex; 
 `;
 
+const ToDoListContainer = Styled.div`
+  min-width: 350px;
+  height: 400px;
+  overflow-y: scroll;
+  border: 1px solid #BDBDBD;
+  margin-bottom: 20px;
+`;
+
+
 function App() {
   const [toDo, setToDo] = useState('');
+  const [toDoList, setToDoList] = useState<string[]>([]);
+
+  const addToDo = (): void => {
+    if (toDo) {
+      setToDoList([...toDoList, toDo]);
+      setToDo('');
+    }
+  };
+
+  const deleteToDo = (index: number): void => {
+    let list = [...toDoList];
+    list.splice(index, 1);
+    setToDoList(list);
+  };
 
   return (
     <Container>
       <Contents>
-        <ToDoItem label={"추가된 할 일"} onDelete={() => alert('삭제')} />
+        <ToDoListContainer data-testid='toDoList'>
+          {toDoList.map((item, index) => (
+              <ToDoItem key={item} label={item} onDelete={() => deleteToDo(index)} />
+          ))}
+        </ToDoListContainer>
         <InputContainer>
           <Input placeholder="할 일을 입력해 주세요" onChange={(text) => setToDo(text)} />
-          <Button label="추가" onClick={() => alert(toDo)} />
+          <Button label="추가" onClick={addToDo} />
         </InputContainer>
       </Contents>
     </Container>
